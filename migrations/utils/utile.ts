6ac65b8701a0a4f3de907991f2776c06b61cf244
okonomiyakki <parkji12532@naver.com>
2024-11-26 01:18:39 +0900

    #9 feat(migration): 테이블 고유 id 및 생성 시간 컬럼 초기화 함수

---
+++ migrations/utils/utile.ts
@@ -0,0 +1,24 @@
+import { TableColumnOptions } from 'typeorm';
+
+export const generatePrimaryColumn = (comment: string = 'ID'): TableColumnOptions => {
+  return {
+    name: 'id',
+    type: 'int',
+    unsigned: true,
+    isPrimary: true,
+    isNullable: false,
+    isGenerated: true,
+    generationStrategy: 'increment',
+    comment,
+  };
+};
+
+export const generateCreatedAtColumn = (comment: string = 'Creation Date'): TableColumnOptions => {
+  return {
+    name: 'created_at',
+    type: 'timestamp',
+    isNullable: false,
+    default: 'CURRENT_TIMESTAMP',
+    comment,
+  };
+};
