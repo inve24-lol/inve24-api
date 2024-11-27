bc209d44354eb752e17cf06a78d49ec3c05b85a2
okonomiyakki <parkji12532@naver.com>
2024-11-27 22:53:00 +0900

    #17 feat(redis): 추상 클래스로 구현하여 레포지토리 레이어 분리

---
+++ src/core/redis/repositories/mail-cache-repository.ts
@@ -0,0 +1,37 @@
+import { IMailCacheRepository } from '@core/redis/abstracts/mail-cache-repository.abstract';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+import Redis from 'ioredis';
+
+@Injectable()
+export class MailCacheRepositoryImpl implements IMailCacheRepository {
+  private readonly redis: Redis;
+
+  constructor(private readonly redisService: RedisService) {
+    this.redis = this.redisService.getOrThrow();
+  }
+
+  async setCertCode(email: string, certCode: number, ttl: number): Promise<string> {
+    try {
+      return await this.redis.set(email, certCode, 'EX', ttl);
+    } catch (error) {
+      throw new InternalServerErrorException('Set failed.');
+    }
+  }
+
+  async getCertCode(email: string): Promise<string | null> {
+    try {
+      return await this.redis.get(email);
+    } catch (error) {
+      throw new InternalServerErrorException('Get failed.');
+    }
+  }
+
+  async delCertCode(email: string): Promise<void> {
+    try {
+      await this.redis.get(email);
+    } catch (error) {
+      throw new InternalServerErrorException('Delete failed.');
+    }
+  }
+}
