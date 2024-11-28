090487b3118f5f2b57c7be080423bc11f5225303
okonomiyakki <parkji12532@naver.com>
2024-11-28 19:30:32 +0900

    #22 fix(db): typeorm 의존성 제거

--- src/core/type-orm/abstracts/user-repository.abstract.ts
+++ src/core/type-orm/abstracts/user-repository.abstract.ts
@@ -1,8 +1,7 @@
-import { DeepPartial } from 'typeorm';
 import { UserEntity } from '@core/type-orm/entities/user.entity';
 
 export abstract class IUserRepository {
-  abstract saveUser(createUser: DeepPartial<UserEntity>): Promise<UserEntity>;
+  abstract saveUser(createUser: Partial<UserEntity>): Promise<UserEntity>;
   abstract findUserByEmail(email: string): Promise<UserEntity | null>;
   abstract findUserByNickname(nickname: string): Promise<UserEntity | null>;
   abstract findUserByUuid(uuid: string): Promise<UserEntity | null>;
