db786167d138f7ac952d9462b4086299ec8c10a4
okonomiyakki <parkji12532@naver.com>
2024-11-28 19:09:10 +0900

    #22 remove(db): Token 레포지토리 클래스 삭제

--- src/core/type-orm/abstracts/token-repository.abstract.ts
+++
@@ -1,8 +0,0 @@
-import { DeleteResult } from 'typeorm';
-import { TokenEntity } from '@core/type-orm/entities/token.entity';
-
-export abstract class ITokenRepository {
-  abstract upsertToken(userUuid: string, refreshToken: string): Promise<void>;
-  abstract findTokenByUserUuid(userUuid: string): Promise<TokenEntity | null>;
-  abstract deleteToken(userUuid: string): Promise<DeleteResult>;
-}
