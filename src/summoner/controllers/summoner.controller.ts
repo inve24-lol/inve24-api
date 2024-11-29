350bd98c513e5ce60fcb771a83d297fb92758c20
okonomiyakki <parkji12532@naver.com>
2024-11-30 00:19:52 +0900

    #28 feat(summoner): RSO 주소 요청 API 구현

--- src/summoner/controllers/summoner.controller.ts
+++ src/summoner/controllers/summoner.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { RiotSignOnUrlResponseDto } from '@summoner/dto/responses/riot-sign-on-url-response.dto';
+import { SummonerService } from '@summoner/services/summoner.service';
 
 @Controller('summoner')
-export class SummonerController {}
+export class SummonerController {
+  constructor(private readonly summonerService: SummonerService) {}
+
+  @ApiOperation({ summary: 'RSO URL 조회' })
+  @ApiOkResponse({ type: RiotSignOnUrlResponseDto })
+  @HttpCode(HttpStatus.OK)
+  @Get('v1/rso-url')
+  riotSignOnUrl(): RiotSignOnUrlResponseDto {
+    return this.summonerService.riotSignOnUrl();
+  }
+}
--- src/summoner/services/summoner.service.ts
+++ src/summoner/services/summoner.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import riotConfig from '@core/config/settings/riot.config';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { RiotSignOnUrlResponseDto } from '@summoner/dto/responses/riot-sign-on-url-response.dto';
+import { IWebClientService } from '@summoner/web-client/abstracts/web-client-service.abstract';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
-export class SummonerService {}
+export class SummonerService {
+  constructor(
+    private readonly webClientService: IWebClientService,
+    @Inject(riotConfig.KEY) private readonly config: ConfigType<typeof riotConfig>,
+  ) {}
+
+  riotSignOnUrl(): RiotSignOnUrlResponseDto {
+    const { auth, redirectUri } = this.config.riot.rso;
+    const { host, authorize, clientId, responseType, scope } = auth;
+
+    const urlParams = new URLSearchParams({
+      client_id: clientId || '',
+      redirect_uri: redirectUri || '',
+      response_type: responseType || '',
+      scope: scope || '',
+    });
+
+    const riotSignOnUrl = `${host}/${authorize}?${urlParams.toString()}`;
+
+    return plainToInstance(RiotSignOnUrlResponseDto, { riotSignOnUrl });
+  }
+}
