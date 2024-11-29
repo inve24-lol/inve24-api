4e4a2e558e912a5ae9c4e3d05cbeaa85d48081d7
okonomiyakki <parkji12532@naver.com>
2024-11-29 20:11:01 +0900

    #26 rename(config): 파일 이름 오타 수정

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -4,7 +4,7 @@ import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { TypeOrmModuleOptionsFactory } from '@core/type-orm/factories/type-orm-module-options.factory';
 import { RedisModuleOptionsFactory } from '@core/redis/factories/redis-module-options.factory';
 import { WebClientModule } from '@core/web-client/web-client.module';
-import { ConfigExModule } from './config/config-ex-module';
+import { ConfigExModule } from './config/config-ex.module';
 
 @Module({
   imports: [
