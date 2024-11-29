f7886a09133a554f649c29e95c9ff11d75f09429
okonomiyakki <parkji12532@naver.com>
2024-11-29 19:22:04 +0900

    #26 feat(summoner): WebClient 모듈 등록

--- src/summoner/summoner.module.ts
+++ src/summoner/summoner.module.ts
@@ -1,8 +1,10 @@
+import { WebClientModule } from '@core/web-client/web-client.module';
 import { Module } from '@nestjs/common';
 import { SummonerController } from '@summoner/controllers/summoner.controller';
 import { SummonerService } from '@summoner/services/summoner.service';
 
 @Module({
+  imports: [WebClientModule],
   controllers: [SummonerController],
   providers: [SummonerService],
 })
