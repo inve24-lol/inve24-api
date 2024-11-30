ed71984bb494b38dd4cb247b8162363e571903df
okonomiyakki <parkji12532@naver.com>
2024-12-01 03:29:49 +0900

    #32 fix(summoner): dto 이름 수정

--- src/summoner/dto/requests/register-summoner-request.dto.ts
+++ src/summoner/dto/requests/register-summoner-request.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class RegisterRequestDto {
+export class RegisterSummonerRequestDto {
   @ApiProperty({
     description: 'RSO 인증 접근 코드',
   })
