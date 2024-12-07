4e01fc1e96ad1bb1efd66162207e87563768a779
okonomiyakki <parkji12532@naver.com>
2024-12-08 07:38:12 +0900

    #36 feat(socket): 게이트웨이 설정 초안
    - 일단 일렉트론 테스트완

---
+++ src/socket/gateways/app-client-socket.gateway.ts
@@ -0,0 +1,114 @@
+import { forwardRef, Inject, UnauthorizedException } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { SummonerService } from '@summoner/services/summoner.service';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: { origin: '*' },
+  namespace: '/ws/socket/app',
+})
+@WebSocketGateway()
+export class AppClientSocketGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(
+    @Inject(forwardRef(() => SummonerService)) private readonly summonerService: SummonerService,
+  ) {}
+
+  @WebSocketServer() private server!: Server;
+
+  afterInit(server: Server) {
+    this.server = server;
+  }
+
+  // puuid로 존재하는 소환사 계정인지 검사 후 연결
+  async handleConnection(@ConnectedSocket() appClient: Socket): Promise<void> {
+    try {
+      const { socketEntryCode } = appClient.handshake.auth;
+
+      if (!socketEntryCode) throw new UnauthorizedException('Auth 형식이 잘못되었습니다.');
+
+      const summoner = await this.summonerService.findSummonerProfileByPuuid(socketEntryCode);
+
+      // if (!summoner)
+      //   throw new UnauthorizedException(
+      //     '플레이 중이신 라이엇 계정이 서비스에 등록되어 있지 않습니다.',
+      //   );
+
+      // const cachedAppClientId = await this.summonerService.getSocketEntryCode(socketEntryCode);
+
+      // if (cachedAppClientId) await this.disconnectOldSocket(socketEntryCode, cachedAppClientId);
+
+      await this.summonerService.setSocketEntryCode(socketEntryCode, appClient.id);
+
+      console.log('기본 방', appClient.rooms);
+
+      appClient.emit('invite-room', { message: '라이엇 계정 인증 성공', data: null });
+    } catch (error) {
+      console.error(`일렉트론 소켓 연결 실패 (id: ${appClient.id})`);
+      appClient.emit('handle-connection-error', error);
+      appClient.disconnect(true);
+    }
+  }
+
+  handleDisconnect(@ConnectedSocket() appClient: Socket): void {
+    console.log(`종료된 일렉트론 소켓 (id: ${appClient.id})`);
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(
+    @ConnectedSocket() appClient: Socket,
+    @MessageBody() body: { roomId: string },
+  ): void {
+    const { roomId } = body;
+
+    appClient.join(roomId);
+
+    console.log('puuid 방 추가', appClient.rooms);
+
+    // this.server
+    //   .to(roomId)
+    //   .emit('join-room-reply-app', { message: '화면에 발급된 QR코드를 찍어주세요.', data: null });
+
+    appClient.emit('join-room-reply-app', {
+      message: '서버 방 참여 성공',
+      data: null,
+    });
+  }
+
+  @SubscribeMessage('disconnect-request')
+  async handleDisconnectRequest(@MessageBody() body: { socketEntryCode: string }): Promise<void> {
+    const { socketEntryCode } = body;
+
+    await this.summonerService.delSocketEntryCode(socketEntryCode);
+  }
+
+  // async disconnectOldSocket(socketEntryCode: string, cachedAppClientId: string): Promise<void> {
+  //   console.log(this.server.sockets.sockets);
+  //   const oldAppClient = this.server.sockets.sockets.get(cachedAppClientId);
+
+  //   if (oldAppClient) {
+  //     await this.summonerService.delSocketEntryCode(socketEntryCode);
+
+  //     console.error(`중복 로그인 감지 (id: ${cachedAppClientId})`);
+
+  //     oldAppClient.emit(
+  //       'session-conflict-error',
+  //       '새로운 PC 접근이 감지되어 현재 연결을 종료합니다.',
+  //     );
+
+  //     oldAppClient.disconnect(true);
+  //   }
+
+  //   return;
+  // }
+}
---
+++ src/socket/gateways/web-client-socket.gateway.ts
@@ -0,0 +1,65 @@
+import { AuthService } from '@auth/services/auth.service';
+import { forwardRef, Inject, UnauthorizedException } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { SummonerService } from '@summoner/services/summoner.service';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: { origin: '*' },
+  namespace: '/ws/socket/web',
+})
+@WebSocketGateway()
+export class WebClientSocketGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(
+    private readonly authService: AuthService,
+    @Inject(forwardRef(() => SummonerService)) private readonly summonerService: SummonerService,
+  ) {}
+
+  @WebSocketServer() private server!: Server;
+
+  afterInit(server: Server) {
+    this.server = server;
+  }
+
+  async handleConnection(@ConnectedSocket() webClient: Socket) {
+    try {
+      const { authorization } = webClient.handshake.headers;
+
+      if (!authorization) throw new UnauthorizedException('Auth 헤더가 존재하지 않습니다.');
+
+      console.log(authorization);
+
+      console.log(`웹 소켓 연결 시작 (id: ${webClient.id})`);
+    } catch (error) {
+      console.error(`에러로 인한 웹 소켓 연결 강제 종료 (id: ${webClient.id})`);
+      webClient.disconnect(true);
+    }
+  }
+
+  handleDisconnect(@ConnectedSocket() webClient: Socket) {
+    console.log(`종료가 감지된 웹 소켓 (id: ${webClient.id})`);
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(@ConnectedSocket() webClient: Socket, @MessageBody() body: { roomId: string }) {
+    const { roomId } = body;
+
+    webClient.join(roomId);
+
+    this.server.to(roomId).emit('join-room-reply-web', {
+      message: '데스크탑 어플리케이션과 연결되었습니다.',
+      data: null,
+    });
+  }
+}
