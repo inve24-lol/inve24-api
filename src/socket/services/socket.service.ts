ce83bbf7adf2f7e8e11450fd100fa03c9f8fb498
okonomiyakki <parkji12532@naver.com>
2024-12-12 17:45:32 +0900

    #42 feat(socket): 웹이랑 앱에 메시지 보내는 기능

--- src/socket/services/socket.service.ts
+++ src/socket/services/socket.service.ts
@@ -1,14 +1,31 @@
 import redisConfig from '@config/settings/redis.config';
-import { Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { ISocketClientCacheRepository } from '@redis/abstracts/socket-client-repository.abstract';
+import { AppClientSocketGateway } from '@socket/gateways/app-client-socket.gateway';
+import { WebClientSocketGateway } from '@socket/gateways/web-client-socket.gateway';
 
 @Injectable()
 export class SocketService {
   constructor(
+    @Inject(forwardRef(() => AppClientSocketGateway)) private app: AppClientSocketGateway,
+    @Inject(forwardRef(() => WebClientSocketGateway)) private web: WebClientSocketGateway,
     private readonly socketClientCacheRepository: ISocketClientCacheRepository,
     @Inject(redisConfig.KEY) private readonly config: ConfigType<typeof redisConfig>,
   ) {}
+
+  async emitToApp(socketEntryCode: string, message: string) {
+    this.app.server.to(socketEntryCode).emit('hello', { message, data: null });
+  }
+
+  async emitToWeb(socketEntryCode: string, message: string) {
+    this.web.server.to(socketEntryCode).emit('hello', { message, data: null });
+  }
+
+  async emitToAll(socketEntryCode: string, message: string) {
+    await this.emitToApp(socketEntryCode, message);
+    await this.emitToWeb(socketEntryCode, message);
+  }
 
   async getSocketStatus(puuid: string): Promise<string | void> {
     const cachedPuuid = await this.socketClientCacheRepository.getSocketStatus(puuid);
